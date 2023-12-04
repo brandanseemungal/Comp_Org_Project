64af169d9d773d13396a57ed7362ce892aacaced
brandanseemungal <112520851+brandanseemungal@users.noreply.github.com>
2023-12-03 19:55:21 -0500

    Update COMP_ORG_PROJECT.asm
    Created text, and Main section that prompt the user for input

--- COMP_ORG_PROJECT.asm
+++ COMP_ORG_PROJECT.asm
@@ -5,4 +5,10 @@ error_msg: .asciiz "Illegal Number!\n"
 fib_msg: .asciiz "\nFibonacci Sequence:\n"
 
     .text
+    .globl main
 
+main:
+    li $v0, 4               # Print the prompt
+    la $a0, prompt
+    syscall
+    
